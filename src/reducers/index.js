613b06d759ce794774e3b18aa31024ca79ee641a
Ahmet Özdemir <ozde.ahmet.611@gmail.com>
2018-12-19 16:55:28 +0300

    Ahmet Özdemir

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import kimlikdogrulamaReducers from './KimlikdogrulamaReducers';
+import StudentsListReducers from './StudentsListReducer';
 
 export default combineReducers({
   kimlikdogrulamaResponse: kimlikdogrulamaReducers,
+  studentsListResponse: StudentsListReducers
 });
