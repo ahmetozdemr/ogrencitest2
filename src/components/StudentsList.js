bd41ccd50d4b50ab29bbeff196bc8b1e58b6c812
Ahmet Özdemir <ozde.ahmet.611@gmail.com>
2018-12-25 16:02:49 +0300

    Data Objesini Dizilere(Array) Aktarma

--- src/components/StudentsList.js
+++ src/components/StudentsList.js
@@ -1,13 +1,24 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { connect } from 'react-redux';
 import { studentsListData } from '../actions';
 
 class StudentsList extends Component {
     componentWillMount(){
         this.props.studentsListData();
+     
+        }
+        componentWillReceiveProps(nextProps){
+            const ds = new ListView.DataSource({
+                rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.datasource = ds.cloneWithRows(nextProps.studentsArray);
     }
+    
     render() {
+        console.log(this.props.studentsArray);
         return (
             <View>
                 <Text> Öğrenci Listesi </Text>
@@ -18,5 +29,11 @@ class StudentsList extends Component {
         );
     }
 }
+const mapStateTopProps = ({ studentsDataResponse }) => {
+     const studentsArray = _.map(studentsDataResponse.data, (val, uid) => {
+  return { ...val, uid }; // { isim: 'Ayşe', syoisim: 'asda', sube: 'asube', uid: 'kq9aasdfdf' }
+     });
+     return { studentsArray };
+}
 
-export default connect(null, { studentsListData })(StudentsList);
+export default connect( mapStateTopProps, { studentsListData })(StudentsList);
