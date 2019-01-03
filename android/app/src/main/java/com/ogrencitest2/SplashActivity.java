46186cd826a6cc7c5b09685a63c10e2ab8047f49
Ahmet Özdemir <ozde.ahmet.611@gmail.com>
2019-01-03 14:48:57 +0300

    splash screen yapmaya çalıştım

---
+++ android/app/src/main/java/com/ogrencitest2/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.ogrencitest2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
